99716565fac6ca5f6a0458c3613b46ca28248610
Kid Max <kidmax285@gmail.com>
2025-02-08 11:49:09 +0600

    added cascadeOnDelete

--- database/migrations/2025_01_23_171243_create_orders_table.php
+++ database/migrations/2025_01_23_171243_create_orders_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,11 +15,11 @@ return new class extends Migration
 
             $table->string('order_number')->unique();
             $table->enum('status', ['pending', 'processing', 'completed', 'failed', 'canceled'])->default('pending');
-            $table->uuid('customer_id'); // Link to a user or customer
+            $table->foreignUuid('customer_id')->constrained('users')->cascadeOnDelete(); // Link to a user or customer
             $table->decimal('total_amount', 10, 2);
-            $table->foreignUuid('invoice_id')->nullable(); // Link to Invoice
-            $table->foreignUuid('payment_id')->nullable(); // Link to Payment
-            $table->foreignUuid('delivery_id')->nullable(); // Link to Delivery
+            $table->foreignUuid('invoice_id')->constrained()->cascadeOnDelete()->nullable(); // Link to Invoice
+            $table->foreignUuid('payment_id')->constrained()->cascadeOnDelete()->nullable(); // Link to Payment
+            $table->foreignUuid('delivery_id')->constrained()->cascadeOnDelete()->nullable(); // Link to Delivery
 
             $table->timestamps();
         });

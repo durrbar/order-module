89d321e0cefca2ec452e5a3ffce39aa3984f8307
Kid Max <kidmax285@gmail.com>
2025-12-14 11:48:23 +0600

    some migrations update

--- database/migrations/2025_02_08_134425_create_order_items_table.php
+++ database/migrations/2025_02_08_134425_create_order_items_table.php
@@ -27,6 +27,6 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('order_items');
+        Schema::dropIfExists('old_order_items');
     }
 };
